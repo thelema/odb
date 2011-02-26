ba2617f0417faf02560fa913913cc0266aa6d1ab
Eric Norige <thelema314@gmail.com>
2011-02-26 15:10:55 -0500

    data structure cleanups, more directory changing, add some debugging printfs

--- odb.ml
+++ odb.ml
@@ -2,25 +2,26 @@
 
 open Batteries_uni
 open Http_client.Convenience
+open Printf
 
 type pkg = {id: string; mutable props: (string * string) list}
-
-(* TODO: verify no bad chars to make command construction safer *)
-let to_pkg str = {id=str; props=[]} 
-let to_pkg_l str l = {id=str; props=l} 
-let get_prop ~p ~n = List.assoc n p.props
+type dep_tree = N of pkg * dep_tree list
 
 let webroot = "http://mutt.cse.msu.edu/"
 let odb_home = (Sys.getenv "HOME") ^ "/.odb"
 
+let get_prop ~p ~n = List.assoc n p.props
 let tarball_uri p = webroot ^ "pkg/" ^ (get_prop ~p ~n:"tarball")
 let deps_uri id = webroot ^ "pkg/dep/" ^ id
+
 
 let split_by pat str = String.nsplit str pat
 let to_alist = split_by "\n" |- List.map 
     (fun s -> String.split s "=" |> Tuple2.map String.trim)
+let get_info id = deps_uri id |> tap (printf "uri:%s\n%!") |> http_get |> to_alist
 
-let get_info p = p.props <- deps_uri p.id |> http_get |> to_alist
+(* TODO: verify no bad chars to make command construction safer *)
+let to_pkg id = {id=id; props=get_info id} 
 
 let get_tarball p = (* TODO: make efficient *)
   let tarball_contents = tarball_uri p |> http_get in
@@ -28,18 +29,16 @@ let get_tarball p = (* TODO: make efficient *)
     (fun oc fn -> IO.nwrite oc tarball_contents; fn)
   (* returns the filename the tarball is stored in *)
   
-let has_dep p = Sys.command ("ocamlfind query " ^ p.id) = 0
-
-type dep_tree = N of pkg * dep_tree list
-  
+let has_dep p = Sys.command ("ocamlfind query " ^ p.id) = 0  
 let get_deps p = get_prop ~p ~n:"deps" |> split_by "," |> List.map to_pkg
 let rec all_deps p = 
-  get_info p;
+  printf "ad: %s = " p.id;
   let ds = get_deps p |> List.filter (has_dep |- not) in
+  printf "%a\n%!" (List.print String.print) (List.map (fun i -> i.id) ds);
   N (p, List.map all_deps ds)
 
 let install p = 
-  if true then Printf.printf "Install: %s\n" p.id
+  if true then printf "Install: %s\n" p.id
   else begin
     let install_dir = "install-" ^ p.id in
     Unix.mkdir install_dir 0o700;
@@ -47,27 +46,25 @@ let install p =
     let tb = get_tarball p in
     if Sys.command ("tar -zxvf " ^ tb) <> 0 then 
       failwith ("Could not extract tarball for " ^ p.id);
+    let tbdir = Sys.files_of "." // Sys.is_directory |> Enum.get |> Option.get in
+    Sys.chdir tbdir;
     if Sys.file_exists "configure" then
       Sys.command ("./configure") |> ignore;
     if Sys.command ("make") <> 0 then
       failwith ("Could not build tarball for " ^ p.id);
     if Sys.command ("OCAMLFIND_DESTDIR="^odb_home^"/lib make install") <> 0 then
       failwith ("Could not install package " ^ p.id);
-    Sys.chdir ".."
+    Sys.chdir "../.."
   end
 
 let install_dep p =
-  if true then Printf.printf "installing: %s\n" p.id else
   let rec loop (N (p,deps)) =
     List.iter loop deps;
     install p
   in
-  let dtree = all_deps p in
-  loop dtree
+  loop (all_deps p)
 
 let () = 
-  let original_dir = Sys.getcwd () in
   if not (Sys.file_exists odb_home) then Unix.mkdir odb_home 0o700;
   Sys.chdir odb_home;
   args () |> Enum.iter (to_pkg |- install_dep);
-  Sys.chdir original_dir
