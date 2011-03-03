f7c0fc582477772797b8d090fb169dcee915ee19
Eric Norige <thelema314@gmail.com>
2011-03-02 20:15:42 -0500

    Remove batteries dependency

--- odb.ml
+++ odb.ml
@@ -1,13 +1,15 @@
-(* ocamlfind ocamlopt -linkpkg -package batteries,netclient -g -w Z odb.ml -o odb *)
+(* ocamlfind ocamlopt -linkpkg -package str,netclient -g -w Z odb.ml -o odb *)
 let webroot = "http://mutt.cse.msu.edu:8081/"
 let odb_home = (Sys.getenv "HOME") ^ "/.odb"
 let cleanup = false
 
-
-open Batteries_uni
 open Http_client.Convenience
 open Printf
 open Str
+
+let (|>) x f = f x
+let (|-) f g x = g (f x)
+let tap f x = f x; x
 
 type pkg = {id: string; mutable props: (string * string) list}
 type dep_tree = N of pkg * dep_tree list
@@ -19,22 +21,22 @@ let tarball_uri p = webroot ^ "pkg/" ^ (get_prop ~p ~n:"tarball")
 let deps_uri id = webroot ^ "pkg/info/" ^ id
 
 
-let split_by pat str = String.nsplit str pat
-let to_alist = split_by "\n" |- List.filter_map 
-    (fun s -> if String.contains s '=' then Some (String.split s "=" |> Tuple2.map String.trim) else None)
-let get_info id = deps_uri id |> tap (printf "uri:%s\n%!") |> http_get |> to_alist
+let to_alist = Str.split (Str.regexp "\n") |- List.filter (fun s -> String.contains s '=') |- List.map (fun s -> match Str.bounded_split (Str.regexp " *= *") s 2 with [k;v] -> (k,v) | _ -> failwith ("Bad line in alist: " ^ s))
+let get_info id = deps_uri id |> tap (printf "uri:%s\n") |> http_get |> to_alist
 
 (* TODO: verify no bad chars to make command construction safer *)
 let to_pkg id = {id=id; props=get_info id} 
 
 let get_tarball p = (* TODO: make efficient *)
   let tarball_contents = tarball_uri p |> http_get in
-  File.with_temporary_out ~suffix:".tgz" 
-    (fun oc fn -> IO.nwrite oc tarball_contents; fn)
+  let fn,oc = Filename.open_temp_file "odb" ".tgz" in
+  output_string oc tarball_contents;
+  close_out oc;
+  fn
   (* returns the filename the tarball is stored in *)
   
 let has_dep p = Sys.command ("ocamlfind query " ^ p.id) = 0 || Sys.command ("which " ^ p.id) = 0
-let get_deps p = get_prop ~p ~n:"deps" |> split_by "," |> List.map to_pkg
+let get_deps p = get_prop ~p ~n:"deps" |> Str.split (Str.regexp ",") |> List.map to_pkg
 let rec all_deps p = 
   let ds = get_deps p |> List.filter (has_dep |- not) in
   N (p, List.map all_deps ds)
@@ -49,7 +51,8 @@ let install p =
     if Sys.command ("tar -zxvf " ^ tb) <> 0 then 
       failwith ("Could not extract tarball for " ^ p.id);
 
-    (Sys.files_of "." // Sys.is_directory) |> Enum.get |> Option.may Sys.chdir;
+    let dirs = (Sys.readdir "." |> Array.to_list |> List.filter Sys.is_directory) in
+    match dirs with [] -> () | h::_ -> Sys.chdir h;
 
     if Sys.file_exists "setup.ml" then begin (* OASIS BUILD *)
       if Sys.command ("ocaml setup.ml -configure") <> 0 then
@@ -71,17 +74,14 @@ let install p =
   end
 
 let install_dep p =
-  let rec loop (N (p,deps)) =
-    List.iter loop deps;
-    install p
-  in
-  loop (all_deps p)
+  let rec loop (N (p,deps)) = List.iter loop deps; install p in
+  all_deps p |> loop 
 
 let pkg_rx = Str.regexp "<a href=.[-a-zA-Z0-9]+.>\\([-a-zA-Z0-9]+\\)</a>"
 let get_pkg str = 
   if Str.string_match pkg_rx str 0 then Str.matched_group 1 str else failwith"bad html"
 let cleanup_list str =
-  String.nsplit str "<td class=\"n\">" |> List.drop 2 |> List.map get_pkg |> String.concat " "
+  Str.split (Str.regexp "<td class=\"n\">") str |> List.tl |> List.tl |> List.map get_pkg |> String.concat " "
 
 let () = 
   if not (Sys.file_exists odb_home) then Unix.mkdir odb_home 0o700;
@@ -90,4 +90,4 @@ let () =
     print_string "Available packages: ";
     deps_uri "" |> http_get |> cleanup_list |> print_endline
   ) else (* install listed packages *)
-    args () |> Enum.iter (to_pkg |- install_dep);
+    Sys.argv |> Array.iter (to_pkg |- install_dep);
