8a2e44ce907666fe6ffd23aef60748c7bacb2bf2
Eric Norige <thelema314@gmail.com>
2011-03-25 13:36:15 -0400

    Reorganize code into logical modules, minor cleanups

--- odb.ml
+++ odb.ml
@@ -15,7 +15,6 @@ let force = ref false
 let force_all = ref false
 let debug = ref false
 
-
 (* micro-stdlib *)
 open Printf
 open Str
@@ -26,112 +25,125 @@ let tap f x = f x; x
 let dtap f x = if !debug then f x; x
 let (//) x y = if x = "" then y else x
 
-
 (* Command line argument handling *)
 let push_install s = to_install := s :: !to_install
-let cmd_line = [
-  "--clean", Set cleanup, 
-  "Cleanup downloaded tarballs and install folders";
-  "--sudo", Set sudo,
-  "Switch to root for installs";
-  "--force", Set force,
-  "Force (re)installation of packages named";
-  "--force-all", Set force_all,
-  "Force (re)installation of dependencies";
-  "--debug", Set debug,
-  "Debug package dependencies";
-]
-		
+let cmd_line = 
+  [ "--clean", Set cleanup, "Cleanup downloaded tarballs and install folders";
+    "--sudo", Set sudo, "Switch to root for installs";
+    "--force", Set force, "Force (re)installation of packages named";
+    "--force-all", Set force_all, "Force (re)installation of dependencies";
+    "--debug", Set debug, "Debug package dependencies"; ]
+    
 let () = parse cmd_line push_install "ocaml odb.ml [-sudo] <packages>";;
 
 (* micro-http library *)
-let http_get_fn ?(silent=true) uri ~fn =
-  let s = if silent then " -s" else "" in
-  if Sys.command ("curl --url " ^ uri ^ " -o " ^ fn ^ s) <> 0 then 
-    failwith ("Curl failed to get " ^ uri)
+module Http = struct 
+  let get_fn ?(silent=true) uri ~fn =
+    let s = if silent then " -s" else "" in
+    if Sys.command ("curl --url " ^ uri ^ " -o " ^ fn ^ s) <> 0 then 
+      failwith ("Curl failed to get " ^ uri)
+	
+  let get uri =
+    if !debug then printf "Getting URI: %s" uri;
+    let fn = Filename.temp_file "odb" ".info" in
+    get_fn uri ~fn;
+    let ic = open_in fn in
+    let len = in_channel_length ic in
+    let ret = String.create len in
+    really_input ic ret 0 len;
+    close_in ic;
+    Unix.unlink fn;
+    ret
+end
 
-let http_get uri =
-  if !debug then printf "Getting URI: %s" uri;
-  let fn = Filename.temp_file "odb" ".info" in
-  http_get_fn uri ~fn;
-  let ic = open_in fn in
-  let len = in_channel_length ic in
-  let ret = String.create len in
-  really_input ic ret 0 len;
-  close_in ic;
-  Unix.unlink fn;
-  ret
-
-(* micro-association list library *)
+(* Type of a package, with its information in a prop list *)
 type pkg = {id: string; mutable props: (string * string) list}
 type dep_tree = N of pkg * dep_tree list
 
-let get_prop ~p ~n = try List.assoc n p.props with Not_found -> ""
-let get_prop_b ~p ~n = try List.assoc n p.props |> bool_of_string with Not_found -> false
-let get_prop_i ~p ~n = try List.assoc n p.props |> int_of_string with Not_found -> -1
+(* micro property-list library *)
+module PL = struct
+  let get ~p ~n = try List.assoc n p.props with Not_found -> ""
+  let get_b ~p ~n = 
+    try List.assoc n p.props |> bool_of_string with Not_found -> false
+  let get_i ~p ~n = 
+    try List.assoc n p.props |> int_of_string with Not_found -> -1
 
-let to_alist = Str.split (Str.regexp "\n") |- List.filter (fun s -> String.contains s '=') |- List.map (fun s -> match Str.bounded_split (Str.regexp " *= *") s 2 with [k;v] -> (k,v) | [k] -> (k,"") | _ -> failwith ("Bad line in alist: " ^ s))
+  let of_string = 
+    Str.split (Str.regexp "\n") 
+    |- List.filter (fun s -> String.contains s '=') 
+    |- List.map (fun s -> match Str.bounded_split (Str.regexp " *= *") s 2 with 
+	| [k;v] -> (k,v) | [k] -> (k,"") 
+	| _ -> failwith ("Bad line in alist: " ^ s))
+end
 
 (* locations of files in website *)
-let tarball_uri p = webroot ^ "pkg/" ^ (get_prop ~p ~n:"tarball")
+let tarball_uri p = webroot ^ "pkg/" ^ (PL.get ~p ~n:"tarball")
 let deps_uri id = webroot ^ "pkg/info/" ^ id
 
 (* wrapper functions to get data from server *)
-let get_info id = deps_uri id |> http_get |> to_alist
+let get_info id = deps_uri id |> Http.get |> PL.of_string
 let get_tarball p =
   let fn = Filename.temp_file "odb" ".tgz" in
-  tarball_uri p |> http_get_fn ~silent:false ~fn:fn;
+  tarball_uri p |> Http.get_fn ~silent:false ~fn:fn;
   fn
 
 (* TODO: verify no bad chars to make command construction safer *)
 let to_pkg id = {id=id; props=get_info id} 
 
 (* Dependency comparison library *)  
-type cmp = GE | EQ | GT (* Add more?  Add &&, ||? *)
-type ver_req = cmp * int list
-type dep = pkg * ver_req option
-let comp x y = function GE -> x >= y | EQ -> x = y | GT -> x > y
+module Dep = struct
+  type cmp = GE | EQ | GT (* Add more?  Add &&, ||? *)
+  type ver_req = cmp * int list
+  type dep = pkg * ver_req option
+  let comp x y = function GE -> x >= y | EQ -> x = y | GT -> x > y
 
-let rec list_cmp = function [],[] -> 0 | 0::t, [] -> list_cmp (t,[]) | [], 0::t -> list_cmp ([],t) | _::_,[] -> 1 | [], _::_ -> -1 | (x::xt), (y::yt) when x=y -> list_cmp (xt, yt) | (x::_), (y::_) -> compare (x:int) y
-let ver_sat req v2 = v2 <> [] && match req with 
-  | None -> true 
-  | Some (c, v1) -> comp (list_cmp (v1, v2)) 0 c
-let parse_ver v = split (Str.regexp_string ".") v |> List.map int_of_string
+  let rec list_cmp = function [],[] -> 0 
+    | 0::t, [] -> list_cmp (t,[]) | [], 0::t -> list_cmp ([],t) 
+    | _::_,[] -> 1 | [], _::_ -> -1 
+    | (x::xt), (y::yt) when x=y -> list_cmp (xt, yt) 
+    | (x::_), (y::_) -> compare (x:int) y
+  let ver_sat req v2 = v2 <> [] && match req with 
+    | None -> true 
+    | Some (c, v1) -> comp (list_cmp (v1, v2)) 0 c
+  let parse_ver v = split (Str.regexp_string ".") v |> List.map int_of_string
 
-let has_dep (p,ver_req) = 
-  let is_library = get_prop_b p "is_library" in
-  let is_program = get_prop_b p "is_program" in
-  let test_lib () = 
+  let test_lib (p, v) = 
     Sys.command ("ocamlfind query -format %v " ^ p.id ^ " > ocaml-ver") = 0 && 
-      open_in "ocaml-ver" |> input_line |> parse_ver |> ver_sat ver_req
-  in
-  let test_prog () = Sys.command ("which " ^ p.id) = 0 in
-  if is_library || is_program then 
-    (is_library && test_lib ()) || (is_program && test_prog ())
-  else test_lib () || test_prog ();;
-let has_dep (p,v) = has_dep (p,v) |> dtap (fun r -> printf "Package %s dependency satisfied: %B\n%!" p.id r)
-let parse_vreq vr = 
-  let l = String.length vr in
-  if vr.[0] = '>' && vr.[1] = '=' then (GE, parse_ver (String.sub vr 2 (l-3)))
-  else if vr.[0] = '>' then (GT, parse_ver (String.sub vr 1 (l-2)))
-  else if vr.[0] = '=' then (EQ, parse_ver (String.sub vr 1 (l-2)))
-  else failwith ("Unknown comparator in dependency, cannot parse version requirement: " ^ vr)
-let whitespace_rx = regexp "[ \t]+"
-let make_dep str = 
-  let str = global_replace whitespace_rx "" str in
-  match bounded_split (regexp_string "(") str 2 with
-    | [pkg; vreq] -> to_pkg pkg, Some (parse_vreq vreq)
-    | _ -> to_pkg str, None
-let get_deps p = get_prop ~p ~n:"deps" |> Str.split (Str.regexp ",") |> List.map make_dep
-let rec all_deps p = 
-  let ds = get_deps p |> List.filter (has_dep |- not) in
-  N (p, List.map (fst |- all_deps) ds)
+    open_in "ocaml-ver" |> input_line |> parse_ver |> ver_sat v
+
+  let test_prog (p, _v) = Sys.command ("which " ^ p.id) = 0
+	
+  let has_dep (p,_ as d) = 
+    let is_library = PL.get_b p "is_library" in
+    let is_program = PL.get_b p "is_program" in
+    if is_library || is_program then 
+      (is_library && test_lib d) || (is_program && test_prog d)
+    else test_lib d || test_prog d;;
+  let has_dep (p,v) = has_dep (p,v) |> dtap (fun r -> printf "Package %s dependency satisfied: %B\n%!" p.id r)
+  let parse_vreq vr = 
+    let l = String.length vr in
+    if vr.[0] = '>' && vr.[1] = '=' then (GE, parse_ver (String.sub vr 2 (l-3)))
+    else if vr.[0] = '>' then (GT, parse_ver (String.sub vr 1 (l-2)))
+    else if vr.[0] = '=' then (EQ, parse_ver (String.sub vr 1 (l-2)))
+    else failwith ("Unknown comparator in dependency, cannot parse version requirement: " ^ vr)
+  let whitespace_rx = regexp "[ \t]+"
+  let make_dep str = 
+    let str = global_replace whitespace_rx "" str in
+    match bounded_split (regexp_string "(") str 2 with
+      | [pkg; vreq] -> to_pkg pkg, Some (parse_vreq vreq)
+      | _ -> to_pkg str, None
+  let get_deps p = 
+    PL.get ~p ~n:"deps" |> Str.split (Str.regexp ",") |> List.map make_dep
+  let rec all_deps p = 
+    let ds = get_deps p |> List.filter (has_dep |- not) in
+    N (p, List.map (fst |- all_deps) ds)
+end
 
 let run_or ~cmd ~err = if Sys.command cmd <> 0 then raise err
 
 (* Installing a package *)
 let install ?(force=false) p = 
-  if not force && has_dep (p,None) then (
+  if not force && Dep.has_dep (p,None) then (
     print_endline ("Package " ^ p.id ^ " already installed, use --force to reinstall");
   ) else begin
     let install_dir = "install-" ^ p.id in
@@ -144,7 +156,7 @@ let install ?(force=false) p =
     let dirs = (Sys.readdir "." |> Array.to_list |> List.filter Sys.is_directory) in
     (match dirs with [] -> () | h::_ -> Sys.chdir h);
 
-    let as_root = get_prop_b p "install_as_root" || !sudo in
+    let as_root = PL.get_b p "install_as_root" || !sudo in
     let config_opt = if as_root then "" else " --prefix " ^ odb_home in
     let install_pre = 
       if as_root then "sudo " else "OCAMLFIND_DESTDIR="^odb_lib^" " in
@@ -167,7 +179,7 @@ let install ?(force=false) p =
       run_or ~cmd:(install_pre ^ "make install") ~err:install_fail;
     end;
     Sys.chdir odb_home;
-    if not (has_dep (p,None)) then (
+    if not (Dep.has_dep (p,None)) then (
       print_endline ("Problem with installed package: " ^ p.id);
       print_endline ("Installed package is not available to the system");
       print_endline ("Make sure "^odb_home^"/bin is in your PATH");
@@ -182,9 +194,10 @@ let install_dep p =
     List.iter (loop ~force:!(force_all)) deps; 
     install ~force p 
   in
-  all_deps p |> loop ~force:(!force || !force_all)
+  Dep.all_deps p |> loop ~force:(!force || !force_all)
 
 (* Parse directory listing from server to get list of packages *)
+(* Dirty, dirty code, shield your eyes from damage *)
 let pkg_rx = Str.regexp "<a href=.[-\\./a-zA-Z0-9]+.>\\([-a-zA-Z0-9]+\\)</a>"
 let get_pkg str = 
   if Str.string_match pkg_rx str 0 
@@ -207,7 +220,7 @@ let () =
     Sys.command ("rm -rf install-*") |> ignore;  
   if !to_install = [] && not !cleanup then ( (* list packages to install *)
     print_string "Available packages: ";
-    (webroot ^ "pkg/info") |> http_get |> cleanup_list |> print_endline
+    (webroot ^ "pkg/info") |> Http.get |> cleanup_list |> print_endline
   ) else (* install listed packages *)
     List.iter (to_pkg |- install_dep) !to_install
 ;;
